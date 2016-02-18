f11c7a34e7e39b5616b7bf93bc4c6b1da32b730a
Renato Biancalana da Silva <renato.biancalana.silva@gmail.com>
2016-02-18 16:12:55 -0200

    Commit de teste

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -51,6 +51,7 @@
       </div>
     </nav>
   </div>
+  <h1>Testando auto-build em prod</h1>
   <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
       <div class="container title-container">
