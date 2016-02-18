2a2cb852625b3d815c414ec1d1557d6189c7c2f6
Renato Biancalana da Silva <renato.biancalana.silva@gmail.com>
2016-02-18 03:00:05 -0200

    Commit de teste

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -51,7 +51,6 @@
       </div>
     </nav>
   </div>
-  <h1>Último teste</h1>
   <div id="index-banner" class="parallax-container">
     <div class="section no-pad-bot">
       <div class="container title-container">
